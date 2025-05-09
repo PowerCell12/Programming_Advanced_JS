bfc859f510cd6600067c310e55c461c783c57023
Ivan <87208452+PowerCell12@users.noreply.github.com>
2025-05-09 13:27:52 +0300

    Create 05. Math Operations.js

---
+++ Syntax, Functions and Statements/Lab/05. Math Operations.js
@@ -0,0 +1,22 @@
+function OperationOnNums(num1, num2, operator){
+
+
+    if (operator === "+"){
+        return num1 + num2;
+    }
+    else if (operator === "-"){
+        return num1 - num2;
+    }
+    else if (operator === "*"){
+        return num1 * num2;
+    }
+    else if (operator === "/"){
+        return num1 / num2;
+    }
+    else if (operator === "%"){
+        return num1 % num2;
+    }
+    else if (operator === "**"){
+        return num1 ** num2;
+    }
+}
