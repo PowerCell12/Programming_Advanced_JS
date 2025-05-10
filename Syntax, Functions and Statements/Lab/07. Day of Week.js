58fbcad2dfd5cf84779981832ccd7a2666c83d5b
Ivan <87208452+PowerCell12@users.noreply.github.com>
2025-05-10 11:40:38 +0300

    Create 07. Day of Week.js

---
+++ Syntax, Functions and Statements/Lab/07. Day of Week.js
@@ -0,0 +1,29 @@
+function WeekDays(string1){
+
+    if (string1 === "Monday"){
+        return 1;
+    }
+    else if (string1 === "Tuesday"){
+        return 2;
+    }
+    else if (string1 === "Wednesday"){
+        return 3;
+    }
+    else if (string1 === "Thursday"){
+        return 4;
+    }
+    else if (string1 === "Friday"){
+        return 5;
+    }
+    else if (string1 === "Saturday"){
+        return 6;
+    }
+    else if (string1 === "Sunday"){
+        return 7;
+    }
+    else{
+        return "error";
+    }
+
+
+}
